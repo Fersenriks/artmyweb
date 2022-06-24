850d82706efa4c60d73980d497d2ad6c5e2c97ca
Vladimir Ivanov <62818788+Fersenriks@users.noreply.github.com>
2022-06-24 21:17:43 +0100

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 export const baseUrl = 'https://gorest.co.in';
-
+// temporarily 
 export const token = '55e4a7929fdbeb0a69c9792be4419724a99c0476c1a4e1da0e1aefec65fe8408';
 
 export const apiEndPoints = {
